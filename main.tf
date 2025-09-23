8223763ee9c1f7c80f3c0fbb895fc692c7deb5b1
pavannayakanti <32892454+pavannayakanti@users.noreply.github.com>
2025-09-23 16:08:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,45 +1,32 @@
-terraform {
-  required_providers {
-    ocm = {
-      source  = "openshift-online/ocm"
-      version = "0.1.3"
-    }
+resource "null_resource" "rosa_cluster" {
+  provisioner "local-exec" {
+    command = <<EOT
+      rosa create cluster \
+        --cluster-name ${var.cluster_name} \
+        --sts \
+        --cluster-admin-password ${var.cluster_admin_password} \
+        --role-arn ${var.role_arn} \
+        --support-role-arn ${var.support_role_arn} \
+        --controlplane-iam-role ${var.controlplane_role_arn} \
+        --worker-iam-role ${var.worker_role_arn} \
+        --operator-roles-prefix ${var.role_prefix} \
+        --tags "${var.cluster_tags}" \
+        --region ${var.aws_region} \
+        --version ${var.openshift_version} \
+        --ec2-metadata-http-tokens optional \
+        --replicas ${var.compute_nodes} \
+        --compute-machine-type ${var.compute_machine_type} \
+        --machine-cidr ${var.machine_cidr} \
+        --service-cidr ${var.service_cidr} \
+        --pod-cidr ${var.pod_cidr} \
+        --host-prefix ${var.host_prefix} \
+        --yes
+    EOT
   }
-}
 
-provider "ocm" {
-  token = var.ocm_token   # Offline token from Red Hat console
-}
-
-resource "ocm_cluster" "iris_ocp4" {
-  name           = var.cluster_name
-  cloud_provider = "aws"
-  cloud_region   = var.aws_region
-  multi_az       = false
-
-  # STS IAM roles
-  sts_role_arn              = var.role_arn
-  sts_support_role_arn      = var.support_role_arn
-  sts_controlplane_role_arn = var.controlplane_role_arn
-  sts_worker_role_arn       = var.worker_role_arn
-
-  # OpenShift version (must use correct ID, e.g., openshift-v4.19.11)
-  openshift_version = var.openshift_version
-
-  # Compute pool
-  compute_nodes        = var.compute_nodes
-  compute_machine_type = var.compute_machine_type
-
-  # AWS account
-  aws_account_id = var.aws_account_id
-
-  # Networking
-  machine_cidr = var.machine_cidr
-  service_cidr = var.service_cidr
-  pod_cidr     = var.pod_cidr
-  host_prefix  = var.host_prefix
-
-  properties = {
-    "training" = "ocp4"
+  # Destroy cluster on terraform destroy
+  provisioner "local-exec" {
+    when    = destroy
+    command = "rosa delete cluster --cluster ${var.cluster_name} --yes --watch"
   }
 }
